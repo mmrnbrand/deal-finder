3c8f160757bc814412d393c554843a888fd5773d
FREEKEY <mmrnbrandbuilders@gmail.com>
2026-06-04 21:37:37 -0400

    update app fix

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def home():
     return render_template("index.html", deals=filtered)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
